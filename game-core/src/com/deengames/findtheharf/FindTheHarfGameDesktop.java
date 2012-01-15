41d05abe83c114a225f99db09139fce2250a1a04
Ashiq A <ashes999@yahoo.com>
2012-01-15 10:31:43 -0500

    Resolution independence. May Allah accept it (ameen).

--- game-core/src/com/deengames/findtheharf/FindTheHarfGameDesktop.java
+++ game-core/src/com/deengames/findtheharf/FindTheHarfGameDesktop.java
@@ -20,6 +20,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
 public class FindTheHarfGameDesktop {
 	public static void main (String[] argv) {
-		new LwjglApplication(new FindTheHarfGame(), "Deen Games", 320, 480, true);
+		new LwjglApplication(new FindTheHarfGame(), "Deen Games", 480, 800, true);
 	}
 }
