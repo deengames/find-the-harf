d766a64d781775c1c500c42b9dcb92a331c0b1c9
Ashiq A <ashes999@yahoo.com>
2012-01-15 10:18:21 -0500

    Forgot to commit....

--- game-core/src/com/deengames/findtheharf/screens/CoreGameScreen.java
+++ game-core/src/com/deengames/findtheharf/screens/CoreGameScreen.java
@@ -14,9 +14,6 @@ import com.deengames.radiantwrench.view.Sprite;
 
 public class CoreGameScreen extends Screen {
 	
-	private int LETTER_WIDTH = 80;
-	private int LETTER_HEIGHT = 68;
-	
 	private String[] _letters = new String[] {
 			"alif", "ba", "ta", "tha", "jeem", "7a", "kha",
 			"daal", "thaal", "ra", "za", "seen", "sheen", "saad",
@@ -34,9 +31,23 @@ public class CoreGameScreen extends Screen {
 		
 		for (int i = 0; i < this._letters.length; i++) {
 			String letter = this._letters[i];
-			Sprite s =this.addSprite("content/images/letters/" + letter + ".png");
-			s.setX(this.getWidth() - (LETTER_WIDTH * (i % 4 + 1)));
-			s.setY(LETTER_HEIGHT * (i / 4));
+			Sprite s = this.addSprite("content/images/letters/" + letter + ".png");
+			
+			float maxWidth = this.getWidth() / 4f;
+			float maxHeight = this.getHeight() / 7f;
+			
+			float wScale = maxWidth / s.getWidth();
+			float hScale = maxHeight / s.getHeight();
+			
+			s.setScale(Math.min(wScale, hScale));
+			
+			// 0.60: accomodate around uber empty space in some letters (like alif)
+			// -32: left-shift (NOT RESOLUTION INDEPENDENT)
+			//s.setX(this.getWidth() - ((int)Math.floor(s.getWidth() * 0.60) * (i % 4 + 1)) - 32);
+			s.setX(this.getWidth() - (s.getWidth() * ((i % 4) + 1)));
+			// -10: up-shift (NOT RESOLUTION INDEPENDENT)
+			//s.setY(s.getHeight() * (i / 4) - 10);
+			s.setY(s.getHeight() * (i / 4));
 		}
 		
 		this.fadeIn();
