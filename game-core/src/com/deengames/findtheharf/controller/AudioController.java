35f7fea9ba247768014b5f0341a2a2a3a0aeca9b
admin <admin@Gateway1.gateway.2wire.net>
2012-01-16 20:22:17 -0500

    Fixed the serial playback (I had been in the middle of comenting out code). It unfortunately plays it in serial; it needs a timer to make it work.

--- game-core/src/com/deengames/findtheharf/controller/AudioController.java
+++ game-core/src/com/deengames/findtheharf/controller/AudioController.java
@@ -1,5 +1,7 @@
 package com.deengames.findtheharf.controller;
 
+import java.io.Console;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
@@ -20,25 +22,32 @@ public class AudioController {
 			play(audioFileNames[0]);
 		} else {
 			// Play in serial
-			
 			int i = 0;
-			Music currentSound = Gdx.audio.newMusic(Gdx.files.internal(audioFileNames[i]));
+			Music currentSound = Gdx.audio.newMusic(Gdx.files.internal(audioFileNames[0]));
 			currentSound.play();
-			//boolean checked = false;
+			boolean checked = false;
 			
 			while (i < audioFileNames.length) {
 				// currentSound.getPosition() of 0 is the only indication 
 				// that the file is complete. But use checked to make sure
 				// we don't accidentally think a sound is done right away.
-				if (currentSound.getPosition() == 0){ // && checked) {
+				System.out.println("if: getPosition=" + currentSound.getPosition() + ", checked=" + checked);
+				if (currentSound.getPosition() == 0 && checked) {
+					System.out.println("GetPos == 0 && checked");
 					i++;
+					System.out.println("i++; i = " + i);
+					System.out.println("if i (" + i + ") < " + audioFileNames.length);
 					if (i < audioFileNames.length) {
 						currentSound = Gdx.audio.newMusic(Gdx.files.internal(audioFileNames[i]));
 						currentSound.play();
-						//checked = false;
+						checked = false;
+						System.out.println("played sound " + i + "; checked = false");
 					}
 				}
-				//checked = (currentSound.getPosition() > 0);
+				
+				System.out.println("checked |= (pos == " + currentSound.getPosition() + ")");
+				checked |= (currentSound.getPosition() > 0);
+				System.out.println("Checked = " + checked);
 			}
 			
 		}
