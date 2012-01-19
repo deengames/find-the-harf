9653398006312046f3c57ec193fdf476e73d1ff5
Ashiq A <ashes999@yahoo.com>
2012-01-19 18:49:54 -0500

    You now get congratulated for picking the right letter. And fixed a bug in the AudioController where it had multiple audio queues. Also made the main screen adjust if width > height, to display horizontally.

--- game-core/src/com/deengames/findtheharf/screens/CoreGameScreen.java
+++ game-core/src/com/deengames/findtheharf/screens/CoreGameScreen.java
@@ -14,14 +14,14 @@ import com.deengames.radiantwrench.view.Sprite;
 
 public class CoreGameScreen extends Screen {
 	
+	String _letterToFind = ""; 
+	
 	private String[] _letters = new String[] {
 			"alif", "ba", "ta", "tha", "jeem", "7a", "kha",
 			"daal", "thaal", "ra", "za", "seen", "sheen", "saad",
 			"daad", "taw", "thaw", "ayn", "ghayn", "fa",
 			"qaaf", "kaaf", "laam", "meem", "noon", "ha", "waw", "ya"
 	};
-	
-	private int _targetLetterIndex;
 	
 	@Override
 	public void initialize() {
@@ -35,27 +35,48 @@ public class CoreGameScreen extends Screen {
 			final String letter = this._letters[i];
 			Sprite s = this.addSprite("content/images/letters/" + letter + ".png");
 			
-			float maxWidth = this.getWidth() / 4f;
-			float maxHeight = this.getHeight() / 7f;
+			int numHorizontal = 4;
+			int numVertical = 7;
+			
+			if (this.getWidth() > this.getHeight()) {
+				numHorizontal = 7;
+				numVertical = 4;
+			}
+			
+			float maxWidth = this.getWidth() / (numHorizontal * 1.0f);
+			float maxHeight = this.getHeight() / (numVertical * 1.0f);
 			
 			float wScale = maxWidth / s.getWidth();
 			float hScale = maxHeight / s.getHeight();
 			
 			s.setScale(Math.min(wScale, hScale));
 			
-			s.setX(this.getWidth() - (s.getWidth() * ((i % 4) + 1)));
-			s.setY(s.getHeight() * (i / 4));
+			s.setX(this.getWidth() - (s.getWidth() * ((i % numHorizontal) + 1)));
+			s.setY(s.getHeight() * (i / numHorizontal));
 			
 			s.setClickListener(new ClickListener() {
 				public void onClick(Clickable clickable) {
-					AudioController.play("content/audio/speech/letters/" + letter + ".mp3");
+					if (_letterToFind != "" && _letterToFind == letter) {
+						AudioController.playInSerial(new String[] { 
+								"content/audio/speech/you-found-the-letter.mp3",
+								"content/audio/speech/letters/" + _letterToFind + ".mp3",
+								"content/audio/speech/good-job.mp3"});
+						
+						findANewLetter();
+					} else {
+						AudioController.play("content/audio/speech/letters/" + letter + ".mp3");
+					}
 				}
 			});			
 		}
 		
-		_targetLetterIndex = MathUtils.random(this._letters.length);		
-		AudioController.playInSerial(new String[] { "content/audio/speech/find-the-letter.mp3", "content/audio/speech/letters/" + this._letters[this._targetLetterIndex] + ".mp3" });
+		findANewLetter();
 		
 		this.fadeIn();
+	}
+	
+	private void findANewLetter() {
+		_letterToFind = _letters[MathUtils.random(this._letters.length)];		
+		AudioController.playInSerial(new String[] { "content/audio/speech/find-the-letter.mp3", "content/audio/speech/letters/" + _letterToFind + ".mp3" });
 	}
 }
