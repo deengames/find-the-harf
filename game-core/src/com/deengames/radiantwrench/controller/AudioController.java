7768b93bd7c72190e73e29cfde405f777304a7a4
ashes999 <ashes999@yahoo.com>
2012-03-18 17:27:55 -0400

    Changes to clear BSODs on Alia's phone. Copied assets over.

--- game-core/src/com/deengames/radiantwrench/controller/AudioController.java
+++ game-core/src/com/deengames/radiantwrench/controller/AudioController.java
@@ -12,48 +12,52 @@ public class AudioController {
 	private static boolean _enabled = true;
 	private static LinkedList<String> _soundQueue = new LinkedList<String>();
 	private static Music _currentSound = null;
-	
+
 	// static class
-	private AudioController() { }
-	
+	private AudioController() {
+	}
+
 	public static void tick() {
 		if (_enabled) {
 			if (_currentSound != null && !_currentSound.isPlaying()) {
 				if (_soundQueue.size() > 0) {
-					String next = _soundQueue.pop();
+					String next = _soundQueue.removeFirst();
 					_currentSound = Gdx.audio.newMusic(Gdx.files.internal(next));
 					_currentSound.play();
 				} else {
 					_currentSound = null;
-				}			
+				}
 			}
 		}
 	}
-	
+
 	public static void play(String audioFileName) {
 		if (_enabled) {
 			Gdx.audio.newSound(Gdx.files.internal(audioFileName)).play();
-		}	
+		}
 	}
-	
+
 	// Currently, everything that plays in serial is in one queue.
 	// TODO: allow multiple queues. Should be easy enough.
 	public static void playInSerial(String[] audioFileNames) {
 		if (_enabled) {
 			if (audioFileNames.length == 0) {
 				return;
-			} else {			
+			} else {
 				int startIndex = 0;
 				// No sounds playing, and:
-					// There's no sound playing, or
-					// The current sound is done (not playing)
-				if (_soundQueue.size() == 0 && (_currentSound == null || (_currentSound != null && !_currentSound.isPlaying()))) {
+				// There's no sound playing, or
+				// The current sound is done (not playing)
+				if (_soundQueue.size() == 0
+						&& (_currentSound == null || (_currentSound != null && !_currentSound
+								.isPlaying()))) {
 					// Play immediately, don't wait for the next tick.
-					_currentSound = Gdx.audio.newMusic(Gdx.files.internal(audioFileNames[0]));
+					_currentSound = Gdx.audio.newMusic(Gdx.files
+							.internal(audioFileNames[0]));
 					_currentSound.play();
 					startIndex = 1;
 				}
-				
+
 				// Queue everything else
 				for (int i = startIndex; i < audioFileNames.length; i++) {
 					_soundQueue.add(audioFileNames[i]);
@@ -68,5 +72,5 @@ public class AudioController {
 		}
 		_soundQueue.clear();
 	}
-	
+
 }
