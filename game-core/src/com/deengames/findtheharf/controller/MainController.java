f9ed5439a05eac2a978b4e11b84d2ca24bca9909
ashes999 <ashes999@yahoo.com>
2017-03-07 12:10:23 -0500

    Remove game-core

--- game-core/src/com/deengames/findtheharf/controller/MainController.java
+++
@@ -1,8 +0,0 @@
-package com.deengames.findtheharf.controller;
-
-
-public class MainController {
-
-	
-	
-}
--- game-core/src/com/deengames/radiantwrench/utils/RadiantWrenchException.java
+++
@@ -1,9 +0,0 @@
-package com.deengames.radiantwrench.utils;
-
-public class RadiantWrenchException extends RuntimeException {
-
-	public RadiantWrenchException(String message) {
-		super(message);
-	}
-
-}
