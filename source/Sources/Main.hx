1e4f9a6029ef9e8c0265f7bd0ccfc12cb6e3e630
ashes999 <ashes999@yahoo.com>
2017-03-08 12:08:15 -0500

    Update to latest Kha from Git Using latest commit available today

--- source/Sources/Main.hx
+++ source/Sources/Main.hx
@@ -6,7 +6,7 @@ class Main {
 	public static function main() {
 		// index.html doesn't use these properties. You can set them by hand
 		// (Kha won't overwrite them when you rebuild the html5 target).
-		System.init("Unknown", 576, 1024, function () {
+		System.init({ title: "Unknown", width: 576, height: 1024 }, function () {
 			new Unknown();
 		});
 	}
