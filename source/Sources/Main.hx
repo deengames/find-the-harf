20b6d7ddc42043f1313deb2afb9eb48bd2955e24
ashes999 <ashes999@yahoo.com>
2017-03-08 10:22:02 -0500

    Document setting canvas size

--- source/Sources/Main.hx
+++ source/Sources/Main.hx
@@ -4,6 +4,8 @@ import kha.System;
 
 class Main {
 	public static function main() {
+		// index.html doesn't use these properties. You can set them by hand
+		// (Kha won't overwrite them when you rebuild the html5 target).
 		System.init("Unknown", 576, 1024, function () {
 			new Unknown();
 		});
--- source/khafile.js
+++ source/khafile.js
@@ -1,4 +1,7 @@
 var project = new Project('Blocks');
 project.addAssets('Assets/**');
 project.addSources('Sources');
+// These don't work in Kha 16.1.2 (from haxelib)
+//project.windowOptions.width = 576;
+//project.windowOptions.height = 1024;
 return project;
