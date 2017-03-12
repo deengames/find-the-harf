7f9569a3da45e91737f9bbf38a41a74a7aed8b1e
ashes999 <ashes999@yahoo.com>
2017-03-12 16:30:02 -0400

    Pick new random target

--- source/source/PlayState.hx
+++ source/source/PlayState.hx
@@ -5,6 +5,7 @@ import flixel.FlxSprite;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import flixel.math.FlxRandom;
 import quasar.core.QuasarState;
 
 class PlayState extends QuasarState
@@ -13,6 +14,9 @@ class PlayState extends QuasarState
 		"daal", "thaal", "ra", "za", "seen", "sheen", "saad",
 		"daad", "taw", "thaw", "ayn", "ghayn", "fa",
 		"qaaf", "kaaf", "laam", "meem", "noon", "ha", "waw", "ya"];
+
+	private var random:FlxRandom = new FlxRandom();
+	private var currentTarget:String;
 
 	private static inline var LETTERS_ACROSS = 4;
 	private static inline var LETTERS_DOWN = 7;
@@ -26,7 +30,6 @@ class PlayState extends QuasarState
 
 		var groupXOffset = (this.width - (LETTERS_ACROSS * LETTER_SIZE)) / 2;
 		var groupYOffset = (this.height - (LETTERS_DOWN * LETTER_SIZE)) / 2;
-		trace('${groupXOffset} = ${this.width} - (4*${LETTER_SIZE}) / 2');
 
 		for (i in 0...letters.length) {
 			var letter = letters[i];
@@ -36,10 +39,24 @@ class PlayState extends QuasarState
 			sprite.x = ((LETTERS_ACROSS - (i % LETTERS_ACROSS)) * sprite.width) - sprite.width + groupXOffset;
 			sprite.y = (Math.floor(i / LETTERS_ACROSS) * sprite.height) + groupYOffset;	
 		}
+
+		this.selectNewTarget();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+	}
+
+	private function selectNewTarget():Void
+	{
+		var next = this.currentTarget;
+		while (next == this.currentTarget)
+		{
+			next = this.random.getObject(this.letters);
+		}
+		
+		trace('New target: ${next}');
+		this.currentTarget = next;
 	}
 }
