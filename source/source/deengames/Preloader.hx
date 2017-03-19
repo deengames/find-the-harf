7d320f971f8bb5bcb05fcc023eafefd0d5d56235
ashes999 <ashes999@yahoo.com>
2017-03-18 22:22:47 -0400

    Fix preloader. Alwyas appears. Extract some constants.

--- source/source/deengames/Preloader.hx
+++ source/source/deengames/Preloader.hx
@@ -1,6 +1,6 @@
 // Adapted from http://gamepopper.co.uk/2014/08/26/haxeflixel-making-a-custom-preloader/
 // Pre-loads assets. Used for Android and Flash.
-package quasar.states;
+package deengames;
 
 import flash.Lib;
 import flash.display.Bitmap;
@@ -8,16 +8,25 @@ import flash.display.BitmapData;
 import flixel.system.FlxPreloader;
 import openfl.display.Sprite;
 
-@:bitmap("assets/images/preloader-logo.png") class LogoImage extends BitmapData { }
+@:bitmap("assets/images/preloader-logo.png")
+class GraphicLogo extends BitmapData { }
 
 class Preloader extends FlxPreloader
 {
-	var logo:Sprite = new Sprite();
+	private static inline var LOGO_WIDTH = 411;
+	private static inline var LOGO_HEIGHT = 512;
 	private static inline var FADE_FROM_PERCENT = 0.75;
+	private static inline var ROTATIONS_PER_SECOND = 2;
+	
+	private var degreesToRotatePerUpdate:Int;
 
-	public function new(minDisplayTime:Float = 0, ?allowedUrls:Array<String>)
+	var logo:Sprite = new Sprite();
+
+	public function new(minDisplayTime:Float = 0.25, ?allowedUrls:Array<String>)
 	{
 		super(minDisplayTime, allowedUrls);
+		degreesToRotatePerUpdate = Std.int(360 * ROTATIONS_PER_SECOND / 60); // 60FPS
+		trace(degreesToRotatePerUpdate);
 	}
 
 	override private function create():Void
@@ -25,16 +34,17 @@ class Preloader extends FlxPreloader
 		this._width = Lib.current.stage.stageWidth;
 		this._height = Lib.current.stage.stageHeight;
 
-		var bitmap = new Bitmap(new LogoImage(0,0));
+		var bitmap = new Bitmap(new GraphicLogo(LOGO_WIDTH, LOGO_HEIGHT));
+
 		// Rotate around origin
-		bitmap.x = -bitmap.width / 2;
-		bitmap.y = -bitmap.height / 2;
+		bitmap.x = -LOGO_WIDTH / 2;
+		bitmap.y = -LOGO_HEIGHT / 2;
 
 		//Sets the graphic of the sprite to a Bitmap object,
 		// which uses our embedded BitmapData class.
 		logo.addChild(bitmap);
-		logo.x = (this._width - logo.width) / 2;
-		logo.y = (this._height - logo.height) / 2;
+		logo.x = (this._width - LOGO_WIDTH) / 2;
+		logo.y = (this._height - LOGO_HEIGHT) / 2;
 
 		// account for negative-coordinates origin
 		logo.x += -bitmap.x;
@@ -44,13 +54,12 @@ class Preloader extends FlxPreloader
 
 	override public function update(percent:Float):Void
 	{
-        trace("pre");
-        
-		// 60 FPS, two rotations per second
-		logo.rotation += 12;
+		// 60 FPS, two rotations per second. This is degrees.
+		logo.rotation += degreesToRotatePerUpdate;
 		// 0-90% = 1.0 alpha
 		// 91-100% = 1.0 => 0
-		if (percent >= FADE_FROM_PERCENT) {
+		if (percent >= FADE_FROM_PERCENT)
+		{
 			logo.alpha = 1 - (percent - FADE_FROM_PERCENT) * (1 / (1 - FADE_FROM_PERCENT));
 		}
 	}
